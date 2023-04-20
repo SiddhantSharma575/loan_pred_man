8392d70222d3a4c56b29ea1fc413968334f18695
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-04-20 14:34:35 +0530

    Some Changes Added

--- loan_pred_management/src/pages/PreviousLoan.js
+++ loan_pred_management/src/pages/PreviousLoan.js
@@ -1,33 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./previous_loan.css"
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
 
 const PreviousLoan = () => {
+    const [prevLoans, setPrevLoans] = useState([]);
+    const [user, setUser] = useState(localStorage.getItem("user"));
+    const [filteredLoan, setFilteredLoans] = useState([])
+
+    const fetchLoans = async () => {
+
+        await getDocs(collection(db, "documents"))
+            .then((querySnapshot) => {
+                const newData = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                setPrevLoans(newData);
+            })
+
+    }
+
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        setUser(JSON.parse(user))
+    }, [localStorage.getItem("user")])
+
+    console.log(user)
+
+    useEffect(() => {
+        fetchLoans()
+    }, [])
+    useEffect(() => {
+        const newLoan = prevLoans.filter((pr) => pr.uid === user.uid);
+        setFilteredLoans(newLoan)
+    }, [prevLoans])
     return (
         <div className='prev_loan_cont'>
-            <div className='each_loan'>
-                <h3>Loan Id</h3>
-                <p>User name</p>
-                <p>Aadhar No</p>
-                <p>Status : Document Uploaded</p>
-            </div>
-            <div className='each_loan'>
-                <h3>Loan Id</h3>
-                <p>User name</p>
-                <p>Aadhar No</p>
-                <p>Status : Document Uploaded</p>
-            </div>
-            <div className='each_loan'>
-                <h3>Loan Id</h3>
-                <p>User name</p>
-                <p>Aadhar No</p>
-                <p>Status : Document Uploaded</p>
-            </div>
-            <div className='each_loan'>
-                <h3>Loan Id</h3>
-                <p>User name</p>
-                <p>Aadhar No</p>
-                <p>Status : Document Uploaded</p>
-            </div>
+            {
+                filteredLoan.map((loan) => (
+                    <div className='each_loan'>
+                        <h3>Loan Id :  {loan.id}</h3>
+                        <p>User name :  {user.name}</p>
+                        <p>Aadhar No :  {loan.adhar_no}</p>
+                        <p>Status  : {loan.status}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- loan_pred_management/src/pages/Team.js
+++ loan_pred_management/src/pages/Team.js
@@ -2,15 +2,43 @@ import React from 'react'
 import Header from '../components/Header'
 import './team.css'
 import Sid54Img from "../images/sid54img.jpg"
-import Sid55Img from "../images/sid55.jpeg";
+import Sid55Img from "../images/sid55.jpg";
 import Anus10 from "../images/anus_10.jpg"
 import ManojSir from "../images/manoj_sir.jpg"
+import deplogo from "../images/csit.jpg"
+import ShilpaMam from "../images/shilpa_mam.jpg"
+import NidhiMam from "../images/nidhi_mam.jpg"
+
 
 
 const Team = () => {
     return (
         <>
             <div className="team_main">
+                <div className='header_section'>
+                    <h3>Special Thanks to : Department of CSIT, AITR Indore</h3>
+                    <img src={deplogo} alt="" />
+                </div>
+                <div className='dept_team'>
+                    <div className="each_person">
+                        <img src={ShilpaMam} alt="" style={{ width: "200px", height: "200px", borderRadius: "50%" }} />
+                        <h3>Dr. Shilpa Bhalerao</h3>
+                        <p>HOD , CSIT Department</p>
+                    </div>
+                    <div className="each_person">
+                        <img src={NidhiMam} alt="" style={{ width: "200px", height: "200px", borderRadius: "50%" }} />
+                        <h3>Prof: Nidhi Nigam</h3>
+                        <p>Project Co-ordinator</p>
+                    </div>
+                    <div className="each_person">
+                        <img src={ManojSir} alt="" style={{ width: "200px", height: "200px", borderRadius: "50%" }} />
+                        <h3>Prof : Manoj Gupta </h3>
+                        <p>Project Guide</p>
+                    </div>
+                </div>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <h3>Project Team</h3>
+                </div>
                 <div className="team_prof">
                     <div className="each_person">
                         <img src={Sid54Img} alt="" style={{ width: "200px", height: "200px", borderRadius: "50%" }} />
@@ -27,12 +55,6 @@ const Team = () => {
                         <h3>Siddhant Sharma</h3>
                         <p>Machine Learning + Back-end</p>
                     </div>
-                </div>
-                <div className="mentor_view">
-                    <img src={ManojSir} alt="" style={{ width: "200px", height: "200px", borderRadius: "50%" }} />
-                    <h3>Manoj Kumar Gupta</h3>
-                    <p>Project Guide</p>
-                    <p>Assistant Professor AITR Indore</p>
                 </div>
             </div>
         </>
